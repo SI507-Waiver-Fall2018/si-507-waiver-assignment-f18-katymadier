83c6200bb4a448c00ded93fdeba41027794fd3c3
Katy Madier <me@katymadier.com>
2018-06-19 19:37:41 -0400

    Update (Part 2 & 3)

--- part2.py
+++ part2.py
@@ -12,28 +12,8 @@ argument=sys.argv[1]
 conn = sqlite3.connect('Northwind_small.sqlite')
 c = conn.cursor()
 
-#list of tables
-# c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-# print(c.fetchall())
-
-# [('Employee',), ('Category',), ('Customer',), ('Shipper',), ('Supplier',), ('Order',), ('Product',), ('OrderDetail',), ('CustomerCustomerDemo',), ('CustomerDemographic',), ('Region',), ('Territory',), ('EmployeeTerritory',)]
-
-#list of column names
-# c.row_factory=sqlite3.Row
-# c.execute('SELECT * FROM "employee"')
-# row = c.fetchone()
-# names = row.keys()
-# print(names)
-
-#order columns
- # ['Id', 'CustomerId', 'EmployeeId', 'OrderDate', 'RequiredDate', 'ShippedDate', 'ShipVia', 'Freight', 'ShipName', 'ShipAddress', 'ShipCity', 'ShipRegion', 'ShipPostalCode', 'ShipCountry']
-
-#Employee columns
- # ['Id', 'LastName', 'FirstName', 'Title', 'TitleOfCourtesy', 'BirthDate', 'HireDate', 'Address', 'City', 'Region', 'PostalCode', 'Country', 'HomePhone', 'Extension', 'Photo', 'Notes', 'ReportsTo', 'PhotoPath']
-
-
 # python3 part2.py customers
-# part2.py customers: prints the list of all customers
+# prints the list of all customers
 
 if argument=="customers":
     table_name = 'Customer'
@@ -46,7 +26,7 @@ if argument=="customers":
 
 
 # python3 part2.py employees
-# part2.py employees: prints the list of all employees
+# prints the list of all employees
 
 if argument=="employees":
     table_name = 'Employee'
@@ -58,8 +38,8 @@ if argument=="employees":
         print("{}    {} {}\n".format(row[0],row[2],row[1]))
 
 
-#  python3 part2.py orders cust=<customer id>
-# part2.py orders cust=<customer id>: prints the list of order dates for all orders placed for the specified customer. Use the customer ID for this command.
+# python3 part2.py orders cust=<customer id>
+# prints the list of order dates for all orders placed for the specified customer. Use the customer ID for this command.
 if argument=="orders":
     if "cust=" in sys.argv[2]:
         id=sys.argv[2][5:]
@@ -72,8 +52,8 @@ if argument=="orders":
         for row in all_rows:
             print("{}\n".format(row[3]))
 
-#  python3 part2.py orders emp=<employee last name>
-# part2.py orders emp=<employee last name>: prints the list of order dates for all orders managed by the specified employee. Use the employee last name for this command.
+# python3 part2.py orders emp=<employee last name>
+# prints the list of order dates for all orders managed by the specified employee. Use the employee last name for this command.
 
     if "emp=" in sys.argv[2]:
         last=sys.argv[2][4:]
@@ -94,23 +74,3 @@ if argument=="orders":
         print("Order dates\n")
         for row in all_rows:
             print("{}\n".format(row[3]))
-
-
-#DIRECTIONS
-# Part 2: Create a Simple Database application
-# For part 2, you will create a program to access information in the Northwind database (included in the repository)
-#
-# FILENAME: part2.py
-#
-# USAGE: the program does different things based on the arguments passed in.
-# part2.py customers: prints the list of all customers
-#
-# part2.py employees: prints the list of all employees
-#
-# part2.py orders cust=<customer id>: prints the list of order dates for all orders placed for the specified customer. Use the customer ID for this command.
-#
-# part2.py orders emp=<employee last name>: prints the list of order dates for all orders managed by the specified employee. Use the employee last name for this command.
-#
-# SAMPLE OUTPUT: The sample output is large, so it's on another page: Sample Output
-#
-# NOTE: Use sqlite3.
--- part3.py
+++ part3.py
@@ -1,8 +1,34 @@
-# these should be the only imports you need
+#Katy Madier | kmadier
+#SI507 Waiver Part 3
 
+# these should be the only imports you need
 import requests
 from bs4 import BeautifulSoup
 
 # write your code here
 # usage should be python3 part3.py
 
+home_data = requests.get("http://michigandaily.com")
+home_content = home_data.text
+home_soup = BeautifulSoup(home_content, "html.parser")
+
+print("Michigan Daily -- MOST READ\n")
+
+#most read articles
+mostRead=home_soup.find_all("div",class_="view-most-read")
+for section in mostRead:
+    for a in section.find_all('a', href=True):
+        title = a.text
+        link = a['href']
+        #get author
+        article_data = requests.get("http://michigandaily.com" + link)
+        article_content = article_data.text
+        article_soup = BeautifulSoup(article_content, "html.parser")
+        if article_soup.find_all("div",class_="byline"):
+            author_section=article_soup.find_all("div",class_="byline")
+            for section in author_section:
+                for a in section.find_all('a', href=True):
+                    author = a.text
+        else:
+            author = "Daily Staff Writer"
+        print("{}\n" "  by " "{}\n".format(title,author))
